fa329ffe818d1d09e373e77c76b28a51c8e1f9a1
t-yamamoto2 <t-yamamoto@cries.co.jp>
2019-06-25 14:14:43 +0900

    コミット

--- app/src/main/java/com/example/bmiapp/MainActivity.kt
+++ app/src/main/java/com/example/bmiapp/MainActivity.kt
@@ -2,9 +2,6 @@ package com.example.bmiapp
 
 import android.os.Bundle
 
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.design.widget.BottomNavigationView
-import android.support.v4.view.ViewPager
 import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -13,8 +10,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        (pager as ViewPager).adapter = TabAdapter(supportFragmentManager, this)
-        tab_layout.setupWithViewPager(pager)
+        pager.adapter = TabAdapter(supportFragmentManager, this)
+        tabLayout.setup
     }
 
 }
--- app/src/main/java/com/example/bmiapp/TabFragment.kt
+++ app/src/main/java/com/example/bmiapp/TabFragment.kt
@@ -1,3 +1,5 @@
+package com.example.bmiapp
+
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
