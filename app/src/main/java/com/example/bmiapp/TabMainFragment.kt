3f67feb8862ef647dde65920c95010ce26a89015
t-yamamoto2 <t-yamamoto@cries.co.jp>
2019-07-02 15:21:14 +0900

    tabのfrafmentを分ける #2

--- app/src/main/java/com/example/bmiapp/TabMainFragment.kt
+++ app/src/main/java/com/example/bmiapp/TabMainFragment.kt
@@ -11,13 +11,10 @@ import android.widget.TextView
 import android.widget.Toast
 import android.content.SharedPreferences
 import android.preference.PreferenceManager
-import android.support.v7.widget.RecyclerView
 import java.text.SimpleDateFormat
 import com.squareup.moshi.*
-import kotlinx.android.synthetic.main.fragment_history.*
 import java.util.*
 import java.util.List
-import android.support.v7.widget.LinearLayoutManager
 
 val moshi = Moshi.Builder().build()
 val type = Types.newParameterizedType(List::class.java,PersonalDataModel::class.java)
@@ -109,27 +106,3 @@ class TabMainFragment: Fragment() {
     }
 }
 
-//履歴タブ
-class TabHistryFragment: Fragment() {
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        super.onCreateView(inflater, container, savedInstanceState)
-        val view = inflater.inflate(R.layout.fragment_history, container, false)
-        return view
-    }
-
-     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-         val rv : RecyclerView? = view.findViewById(R.id.recycler_view)
-
-         val dataStore: SharedPreferences = PreferenceManager.getDefaultSharedPreferences(this.context)
-         val savedData = dataStore.getString("History", "[]")
-         val savedDataList : List<PersonalDataModel>  = listAdapter.fromJson(savedData) as  List<PersonalDataModel>
-
-         val adapter = ViewAdapter(savedDataList)
-         val llm = LinearLayoutManager(this.context)
-
-         rv?.setHasFixedSize(true);
-         rv?.setLayoutManager(llm);
-         rv?.setAdapter(adapter);
-    }
-}
